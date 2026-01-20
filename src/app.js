bca1e1ebcfb84d432e464bac9c2c0cb8b88b95bf
Chandrika Vishwakarma <vickyopklopkl@gmail.com>
2026-01-20 14:21:19 +0530

    exploring HTTP methods

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.use("/", (req, res)=>{
-    res.send("hello")
+app.get("/" , (req, res) => {
+    res.send('this is home page');
+})
+app.get("/user", (req, res) => {
+    res.send({name : "chandrika" , age : 21})
+})
+app.post("/user", (req, res)=>{
+    res.send("user created successfully")
+})
+app.delete("/user" , (req, res) => {
+    res.send("user deleted ")
 })
 app.listen(port,()=>{
     console.log(`server is listening on port ${port}`)

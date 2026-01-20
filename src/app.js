c1cacdd61aa586342bed04892c399afcb1237c76
Chandrika Vishwakarma <vickyopklopkl@gmail.com>
2026-01-20 15:53:31 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,18 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.get("/" , (req, res) => {
-    res.send('this is home page');
-})
-app.get("/user", (req, res) => {
-    res.send({name : "chandrika" , age : 21})
-})
-app.post("/user", (req, res)=>{
-    res.send("user created successfully")
-})
-app.delete("/user" , (req, res) => {
-    res.send("user deleted ")
-})
-app.listen(port,()=>{
-    console.log(`server is listening on port ${port}`)
-})
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(
+  "/user",
+//   these are route handlers 
+  (req, res, next) => {
+    console.log("chandrika 1")
+    // res.send(" 1 User route");
+    next()
+  },
+  (req, res,next ) => {
+    console.log("chandrika 2");
+    // res.send("2 hai ");
+    next()
+  },
+  (req, res,next ) => {
+    console.log("chandrika 3");
+    // res.send("3hai ");
+    next()
+  },
+  (req, res,next ) => {
+    console.log("chandrika 4");
+    res.send("4hai ");
+    next()
+  },
+);
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});
